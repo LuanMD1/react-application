aaa45106df00318e655eb42afd31356f1771dee9
LuanMD1 <luan.mdn1@gmail.com>
2023-11-27 14:01:37 -0300

    Update to github

---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div className='text' class=' bg-slate-900 h-96 w-100'>Hello</div>
+  )
+}
+
+export default Hello;
---
+++ src/components/Home/Homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage () {
+    return (
+        <div>
+        <h1 className='text-4xl text-red-500' >Hello World!</h1>
+      </div>
+    )
+}
+
+export default Homepage;
